b098e270a98fd2e1d953717b08d0367ae9e79f5d
Lemonthings <chrishutchbaby@gmail.com>
2024-05-21 19:14:52 -0400

    GitIntro
    This introduces my partner Ashley Schoonover to the class

---
+++ GitIntro/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h2> Ashley Schoonover</h2><br><br>";
+        //1 - Name, and major / reason for taking the class
+        echo "Ashley, She is taking the class working toward her cyber security degree<br>";
+
+        //2 - Any previous programming experience that they have
+        echo "Ashley has a Masters in Health informatics so she has a good range of experience she learned in school and uses for work.<br>
+            Ashley knows: Java, Javascript, SQL, Python, HTML/CSS and a few others related to data analytics<br>" ; 
+        //3 - Do they have any hobbies?
+        echo "Ashley's favorite hobbies are video games, cooking, and aerial arts<br>";
+
+        //4 - Do they have any plans for the Fall aside from school?
+        echo "there is a wedding that Ashley is a part of and helping with planning<br>";
+
+        //5 - Anything else they'd like the class to know about them.
+        echo "The majority has already been said. Ashley likes to learn, work, and try new things";
+        ?>
+    </body>
+</html>
